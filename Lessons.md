396a97b4adf5b3ebfb76e7a118893a667862f5c1
Jim Kadin <james.kadin@jax.org>
2017-09-13 13:42:22 -0400

    added cmd line args to tuning templates.

--- Lessons.md
+++ Lessons.md
@@ -1,5 +1,6 @@
 ### Some Lessons Learned
 
+* My version of sklearn is 0.18.1
 * Randomness in training.
     * most is in train_test_split and in the classifier (e.g., SGD)
     * as far as I can tell, there is none caused by the gridsearch CV fold
@@ -19,3 +20,36 @@ TrainTestSplit random_state = 78
 Unicode error
 Classifer random_state = 220
 TrainTestSplit random_state = 335
+
+Model Tuning
+    * SGD with 'log' seems hard to get reasonable recall on test set no matter
+    what I try.
+    * I cannot reproduce Yasmine's results for  SGD with 'log', even using
+    the same params she used.
+    * I cannot reproduce Yasmine's results for SGD and 'modified_huber'
+    * I get a little better results with 'modified_huber', (can get recall
+    up to .9), but not great
+    * SGD with 'hinge' and 'invscaling' is pretty good. Recall 90-94 range
+    and precision in 65-70 range
+
+Good params:
+### Best Pipeline Parameters:
+classifier__alpha: 10000
+classifier__eta0: 1e-05
+classifier__learning_rate: 'invscaling'
+classifier__loss: 'hinge'
+classifier__penalty: 'l2'
+vectorizer__max_df: 0.98
+vectorizer__min_df: 2
+vectorizer__ngram_range: (1, 3)
+
+Also these seem comparable:
+### Best Pipeline Parameters:
+classifier__alpha: 1000
+classifier__eta0: 0.0001
+classifier__learning_rate: 'invscaling'
+classifier__loss: 'hinge'
+classifier__penalty: 'l2'
+vectorizer__max_df: 0.98
+vectorizer__min_df: 2
+vectorizer__ngram_range: (1, 3)
--- ModelTuning/TuningTemplate.py
+++ ModelTuning/TuningTemplate.py
@@ -1,6 +1,7 @@
 
 import sys
 sys.path.append('..')
+import argparse
 import textTuningLib as tl
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
@@ -8,10 +9,10 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 #-----------------------
 def process():
-    randForSplit,randForClassifier = parseArgs()
+    args = parseCmdLine()
     randomSeeds = tl.getRandomSeeds( { 	# None means generate a random seed
-			'randForSplit'      : randForSplit,
-			'randForClassifier' : randForClassifier,
+			'randForSplit'      : args.randForSplit,
+			'randForClassifier' : args.randForClassifier,
 			} )
     beta=4		# >1 weighs recall more than precision
     pipeline = Pipeline( [
@@ -27,30 +28,33 @@ def process():
     parameters={'vectorizer__ngram_range':[(1,3)],
 		'vectorizer__min_df':[2],
 		'vectorizer__max_df':[.98],
-		'classifier__alpha':[.01],
+		'classifier__alpha':[10000],
 		'classifier__learning_rate':['invscaling'],
-		'classifier__eta0':[.1],
-		'classifier__loss':[ 'log' ], # 'log' = Logistic Regression
-		'classifier__penalty':['l1'],
-		'classifier__n_iter':[5],
+		'classifier__eta0':[.00001],
+		'classifier__loss':[ 'hinge' ],
+		'classifier__penalty':['l2'],
+		#'classifier__n_iter':[10],
 		}
     ht = tl.TextPipelineTuningHelper( \
 	pipeline, parameters, beta=beta, cv=5, randomSeeds=randomSeeds,
 	).fit()
-    print ht.getReports()
+    print ht.getReports(verbose=args.verbose)
 #-----------------------
-def parseArgs():
+def parseCmdLine():
     """
-    Support two command line args:
-      <random seed for train_test_split> <random seed for the classifier>
-	 if these are not ints, seeds default to "None" (use random seed)
+    Usage: scriptname [-v] [ randForSplit [ randForClassifier] ]
     """
-    randForSplit = None
-    randForClassifier = None
-    if len(sys.argv) > 1:
-	if sys.argv[1].isdigit(): randForSplit = int(sys.argv[1])
-    if len(sys.argv) > 2:
-	if sys.argv[2].isdigit(): randForSplit = int(sys.argv[2])
-    return randForSplit, randForClassifier
+    parser = argparse.ArgumentParser( \
+    description='Run a tuning experiment, log to tuning.log')
+
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+			help='verbose: print longer tuning report')
+
+    parser.add_argument('randForSplit', nargs='?', default=None, type=int,
+			help="random seed for test_train_split")
+
+    parser.add_argument('randForClassifier', nargs='?', default=None, type=int,
+			help="random seed for classifier")
+    return parser.parse_args()
 #-----------------------
 if __name__ == "__main__": process()
