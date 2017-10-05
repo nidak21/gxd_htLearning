f8bf288bdedf3acbe3d351fe263d17585295d425
Jim Kadin <james.kadin@jax.org>
2017-10-05 16:31:19 -0400

    Rename getCurationStats

--- GetData/getCurationStats.sql
+++
@@ -1,5 +0,0 @@
-SELECT ht._evaluatedby_key, u.name, t.term, count(*) AS "count"
-FROM gxd_htexperiment ht join MGI_user u on (ht._evaluatedby_key = u._user_key)
-    join voc_term t on (ht._evaluationstate_key = t._term_key)
-GROUP BY ht._evaluatedby_key, u.name, t.term
-order by u.name
