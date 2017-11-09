bbbe2cc9627701b214f3f44027ebeb29360f1c07
Jim Kadin <james.kadin@jax.org>
2017-11-09 12:53:54 -0500

    clean up R script

--- confDistribution.R
+++ confDistribution.R
@@ -2,7 +2,7 @@
 
 graphConfTrainOrTest = function(filename)
 {
-    # Graph conf distribution for predictions for the training and test sets.
+    # Graph confidence distributions for predictions for training or test sets.
     # Assume file is tab delimited, with header line,
     # Structure:
     #  Sample (name),
@@ -11,41 +11,65 @@ graphConfTrainOrTest = function(filename)
     #  FN/FP (string: "FP", "FN", or ""),
     #  Confidence (float),
     #  Abs Value of the confidence (float)
+    #
+    # Display 3 distribution histograms in one output window:
+    #   distribution of confidences for all predictions
+    #   distribution of confidences for true predictions
+    #   distribution of confidences for false (FP or FN) predictions
+    # All three have the same x axis for comparison (but y axes differ)
 
     ds = read.table(filename, header=TRUE, sep='\t', row.names="Sample")
 
+    # define relevant subsets of the data
     truePreds = subset(ds, ds[,3] == "")	# true
     FP = subset(ds, ds[,3] == "FP")		# false positives
     FN = subset(ds, ds[,3] == "FN")		# false negatives
     FPFN = rbind(FP, FN)			# all FP FN together
 
-    maxX = max(ds$Abs.value)
-    ticksX = seq(floor(-maxX),ceiling(maxX),0.05)
-    lwd0 = 1.5				# y-axis line width at x=0
-    Ymult = 1.3
-    title = paste("Confidence Values for Known Samples",filename,date(),sep="\n")
+    # define x axis params
+    xMax = max(ds$Abs.value)
+    xTicks = seq(floor(-xMax),ceiling(xMax),0.05) # where tick lines go
+    histGroups = 40		# number of histogram boxes
 
+    # define y axis params
+    yLwd0 = 1.5		# y-axis line width at x=0
+    yMult = 1.3		# y max val multiplier to get y axis to stick up a bit
+
+    # set up graphs (3 per page)
+    title =paste("Confidence Values for Known Samples",filename,date(),sep="\n")
     par(mfrow = c(3,1))				# 3 graphs, 1 column
-    maxY = max(hist(ds$Confidence,breaks=40,plot=FALSE)$counts)
-    hist(ds$Confidence,breaks=40, xlim=c(-maxX,maxX), main=title, 
-	    xlab="All Predictions", xaxt='n',ylim=c(0,Ymult*maxY), col='yellow')
-    axis(1, at=ticksX)
-    axis(2, pos=0, col='black', tck=0, lwd=lwd0, labels=FALSE) # y axis at 0
 
-    maxY = max(hist(truePreds$Confidence,breaks=40,plot=FALSE)$counts)
-    hist(truePreds$Confidence,breaks=40, xlim=c(-maxX,maxX), main="",
-	    ylim=c(0,Ymult*maxY),xlab="True Predictions", xaxt='n', col='green')
-    axis(1, at=ticksX)
-    axis(2, pos=0, col='black', tck=0, lwd=lwd0, labels=FALSE) # y axis at 0
+    # for each graph, we run hist to get the histogram values (without plotting)
+    #  so we can get the yMax for the graph.
+    # We use yMax to customize the y axis.
+    # Then we run hist again to actually to the plot.
+
+    # plot all predictions
+    yMax = max(hist(ds$Confidence,breaks=histGroups,plot=FALSE)$counts)
+
+    hist(ds$Confidence,breaks=histGroups, xlim=c(-xMax,xMax), main=title, 
+	    xlab="All Predictions", xaxt='n',ylim=c(0,yMult*yMax), col='yellow')
+    axis(1, at=xTicks)
+    axis(2, pos=0, col='black', tck=0, lwd=yLwd0, labels=FALSE) # y axis at 0
+
+    # plot true predictions
+    yMax = max(hist(truePreds$Confidence,breaks=histGroups,plot=FALSE)$counts)
+
+    hist(truePreds$Confidence,breaks=histGroups, xlim=c(-xMax,xMax), main="",
+	    ylim=c(0,yMult*yMax),xlab="True Predictions", xaxt='n', col='green')
+    axis(1, at=xTicks)
+    axis(2, pos=0, col='black', tck=0, lwd=yLwd0, labels=FALSE) # y axis at 0
     
-    maxY = max(hist(FPFN$Confidence,breaks=40,plot=FALSE)$counts)
-    hist(FPFN$Confidence, breaks=40, xlim=c(-maxX,maxX), main="",
-	    ylim=c(0,Ymult*maxY), xlab="FP and FN", xaxt='n', col='red')
-    axis(1, at=ticksX)
-    axis(2, pos=0, col='black', tck=0, lwd=lwd0, labels=FALSE) # y axis at 0
+    # plot false predictions
+    yMax = max(hist(FPFN$Confidence,breaks=histGroups,plot=FALSE)$counts)
+
+    hist(FPFN$Confidence, breaks=histGroups, xlim=c(-xMax,xMax), main="",
+	    ylim=c(0,yMult*yMax), xlab="FP and FN", xaxt='n', col='red')
+    axis(1, at=xTicks)
+    axis(2, pos=0, col='black', tck=0, lwd=yLwd0, labels=FALSE) # y axis at 0
 }
 
-graphConfPredictions = function(filename)
+graphConfPredictions = function(filename, xmax=0)
 {
     # Graph confidence distribution for predictions for unknown set
     #    (no "True" classifications are known)
@@ -58,17 +82,20 @@ graphConfPredictions = function(filename)
 
     ds = read.table(filename, header=TRUE, sep='\t', row.names="Sample")
 
-    lwd0 = 1.5				# y-axis line width at x=0
-    Ymult = 1.3
-    maxY = max(hist(ds$Confidence,breaks=40,plot=FALSE)$counts)
-    maxX = max(ds$Abs.value) + .5
-    title = paste("Confidence Values for Unknown Samples",filename,date(),sep="\n")
+    # x-axis params
+    if (xmax==0)xMax = max(ds$Abs.value) + .5
+    else	xMax = xmax
+    histGroups = 40		# number of histogram boxes
 
-    # cannot for the life of me figure out how to display axis x=0, 
-    #  there must be some way..
+    # y-axis params
+    yLwd0 = 1.5		# y-axis line width at x=0
+    yMult = 1.3		# y max val multiplier to get y axis to stick up a bit
 
-    # maxY=c(0,40),	# set maxY?
-    hist(ds$Confidence,breaks=40, xlim=c(-maxX, maxX), main=title, 
-	xlab="All Predictions", ylim=c(0,Ymult*maxY),col='blue')
-    axis(2, pos=0, col='black', tck=0, lwd=lwd0, labels=FALSE) # y axis at 0
+    # find yMax from the histogram (w/o plotting)
+    yMax = max(hist(ds$Confidence,breaks=histGroups,plot=FALSE)$counts)
+
+    title = paste("Confidence Values for Unknowns",filename,date(),sep="\n")
+    hist(ds$Confidence,breaks=histGroups, xlim=c(-xMax, xMax), main=title, 
+	xlab="Predictions for unknown samples", ylim=c(0,yMult*yMax),col='blue')
+    axis(2, pos=0, col='black', tck=0, lwd=yLwd0, labels=FALSE) # y axis at 0
 }
